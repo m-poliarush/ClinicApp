27303324bef3c41c221cff38b58f40108db9baa9
Makysm Poliarush <polarysh2222@gmail.com>
2025-05-30 11:45:15 +0300

    Created DAL with entities

---
+++ Backend/DomainData/DB/ClinicContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainData.DB.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainData.DB
+{
+    public class ClinicContext : DbContext
+    {
+
+        DbSet<User> Users { get; set; }
+        DbSet<Doctor> Doctors { get; set; }
+        DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql("Host=localhost;Port=5432;Database=ClinicDB;Username=postgres;Password=12345;Include Error Detail=true");
+            options.EnableSensitiveDataLogging();
+        }
+
+
+    }
+}
---
+++ Backend/DomainData/DB/Entities/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainData.DB.Entities
+{
+    public class Appointment
+    {
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+
+    }
+}
