8a6936e0a72f25de834b69d90fe4cec751210468
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-01 18:46:09 +0300

    Added Unit of Work

---
+++ Backend/DomainData/UoW/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainData.DB.Entities;
+using DomainData.Repository;
+
+namespace DomainData.UoW
+{
+    public interface IUnitOfWork
+    {
+        public IGenericRepository<User> UsersRepository { get; }
+        public IGenericRepository<Doctor> DoctorsRepository { get; }
+        public IGenericRepository<Appointment> AppointmentsRepository { get; }
+        public void Save();
+    }
+}
---
+++ Backend/DomainData/UoW/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using DomainData.DB;
+using DomainData.DB.Entities;
+using DomainData.Repository;
+
+namespace DomainData.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool disposedValue;
+
+        private readonly ClinicContext clinicContext;
+        private IGenericRepository<User> _usersRepository;
+        private IGenericRepository<Doctor> _doctorsRepository;
+        private IGenericRepository<Appointment> _appointmetsRepository;
+
+        public IGenericRepository<User> UsersRepository => _usersRepository ??= new GenericRepository<User>(clinicContext);
+        public IGenericRepository<Doctor> DoctorsRepository => _doctorsRepository ??= new GenericRepository<Doctor>(clinicContext);
+        public IGenericRepository<Appointment> AppointmentsRepository => _appointmetsRepository ??= new GenericRepository<Appointment>(clinicContext);
+
+
+        public UnitOfWork(ClinicContext context)
+        {
+            this.clinicContext = context;
+        }
+
+        public void Save()
+        {
+            clinicContext.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    clinicContext.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~UnitOfWork()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
