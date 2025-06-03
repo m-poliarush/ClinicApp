0ecdabba57101d1e2e188db5a5a93efd5d75a5a0
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-04 02:42:25 +0300

    Created business models for entities

---
+++ Backend/Application/Models/AppointmentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class AppointmentModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int DoctorId { get; set; }
+    }
+}
---
+++ Backend/Application/Models/ESpecializations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public enum ESpecializations
+    {
+        Therapist,
+        Surgeon,
+        Cardiologist,
+        Neurologist,
+        Dermatologist,
+        Oncologist,
+        Ophthalmologist,
+        Psychiatrist,
+        Dentist
+    }
+}
