b82e29964cce892d5310f03cd1ea4fa441e90e5f
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-04 02:40:01 +0300

    Added Automapper profiles

---
+++ Backend/Profiles/AppointmentProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using AutoMapper;
+using DomainData.DB.Entities;
+
+namespace Profiles
+{
+    public class AppointmentProfile : Profile
+    {
+        public AppointmentProfile()
+        {
+            CreateMap<Appointment, AppointmentModel>().ReverseMap();
+        }
+    }
+}
---
+++ Backend/Profiles/DoctorProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using AutoMapper;
+using DomainData.DB.Entities;
+
+namespace Profiles
+{
+    public class DoctorProfile : Profile
+    {
+        public DoctorProfile() {
+
+            CreateMap<Doctor, DoctorModel>().ReverseMap();
+        
+            
+        }
+    }
+}
