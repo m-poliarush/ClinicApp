0ec5e4502fa90683293ac222ecb5ef39f0a37c2b
06AndriiSK06 <skirdenkoandrej7@gmail.com>
2025-06-05 17:31:44 +0300

    added all

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,15 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+// import Home from "./Pages/Home.tsx";
+import { AuthProvider } from "./Context/AuthContext.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AuthProvider } from './Context/AuthContext.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-    <App />
+      {/* <Home /> */}
+      <App />
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
