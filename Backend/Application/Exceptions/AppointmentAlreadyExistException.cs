0867379466c7bf0939176540184eab15a09266ca
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-05 18:01:51 +0300

    Added Exceptions

---
+++ Backend/Application/Exceptions/AppointmentAlreadyExistException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class AppointmentAlreadyExistException : Exception
+    {
+        public AppointmentAlreadyExistException()
+        {
+            
+        }
+        public AppointmentAlreadyExistException(string message) : base(message) { }
+    }
+}
---
+++ Backend/Application/Exceptions/UserAlreadyExistException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class UserAlreadyExistException : Exception
+    {
+        public UserAlreadyExistException() { }
+        public UserAlreadyExistException(string message) : base(message) { }
+
+    }
+}
