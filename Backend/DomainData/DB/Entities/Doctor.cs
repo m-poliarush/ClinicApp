834b71c43f4c7b69abb483e7ff7bc6f103df51b7
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-05 18:12:49 +0300

    Fix entities

--- Backend/DomainData/DB/Entities/Doctor.cs
+++ Backend/DomainData/DB/Entities/Doctor.cs
@@ -10,6 +10,8 @@ namespace DomainData.DB.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
         public string Specialization { get; set; }
 
     }
---
+++ Backend/DomainData/Migrations/20250604191426_newMigration2.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DomainData.Migrations
+{
+    /// <inheritdoc />
+    public partial class newMigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Doctors_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Users_UserId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Doctors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Doctors");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_DoctorId",
+                table: "Appointments",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_UserId",
+                table: "Appointments",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Doctors_DoctorId",
+                table: "Appointments",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Users_UserId",
+                table: "Appointments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
