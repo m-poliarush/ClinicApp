9dcb5a5c962daf84cd3be40add14157cfbf9002e
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-05 18:11:32 +0300

    Modify profiles

--- Backend/Profiles/DoctorProfile.cs
+++ Backend/Profiles/DoctorProfile.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Application.Models;
 using AutoMapper;
 using DomainData.DB.Entities;
+using DTOs;
 
 namespace Profiles
 {
@@ -14,8 +15,26 @@ namespace Profiles
         public DoctorProfile() {
 
             CreateMap<Doctor, DoctorModel>().ReverseMap();
-        
-            
+
+            CreateMap<DoctorModel, DoctorDTO>()
+                .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.imagePath, opt => opt.MapFrom(src => src.ImagePath))
+                .ForMember(dest => dest.description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.specialization, opt => opt.MapFrom(src => src.Specialization.ToString()));
+
+            CreateMap<DoctorDTO, DoctorModel>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.name))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.ImagePath, opt => opt.MapFrom(src => src.imagePath))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.description))
+                .ForMember(dest => dest.Specialization, opt => opt.MapFrom(src => ParseSpecialization(src.specialization)));
+        }
+        private ESpecializations ParseSpecialization(string specialization)
+        {
+            return Enum.TryParse<ESpecializations>(specialization, true, out var result)
+                ? result
+                : ESpecializations.Therapist;
         }
     }
 }
--- Backend/Profiles/UserProfile.cs
+++ Backend/Profiles/UserProfile.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Models;
 using AutoMapper;
 using DomainData.DB.Entities;
+using DTOs;
 
 namespace Profiles
 {
@@ -13,7 +15,13 @@ namespace Profiles
     {
 
         public UserProfile() {
-            CreateMap<User, UserModel>().ReverseMap();
+            CreateMap<User, UserModel>();
+            CreateMap<UserModel, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+            CreateMap<UserModel, UserDTO>()
+                .ForMember(dest => dest.role, opt => opt.MapFrom(src => src.Role))
+                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.UserName)).ReverseMap();
 
         }
     }
