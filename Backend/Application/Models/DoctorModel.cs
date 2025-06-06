ce18b9b9968e4beda900087e4fdd010e6a072567
Makysm Poliarush <polarysh2222@gmail.com>
2025-06-06 15:54:51 +0300

    Edited models

--- Backend/Application/Models/DoctorModel.cs
+++ Backend/Application/Models/DoctorModel.cs
@@ -10,8 +10,6 @@ namespace Application.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImagePath {  get; set; }
         public ESpecializations Specialization { get; set; }
     }
 }
